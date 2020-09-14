ebed66b38e900ee9b278221fac511b50d31021f5
tarora06 <59690247+tarora06@users.noreply.github.com>
2020-09-14 20:01:11 +0530

    Update

--- leetcode142.java
+++ leetcode142.java
@@ -35,3 +35,41 @@ public:
         
     }
 };
+
+
+// calcuate m according to the questipon x=z+(y+z)(m-1);
+class Solution {
+    public:
+        ListNode *detectCycle(ListNode *head) {
+            if(head==nullptr || head->next==nullptr){
+                return nullptr;
+            }
+            ListNode *slow=head;
+            ListNode *fast=head;
+            ListNode *pos=head;
+            while(fast!=nullptr && fast->next!=nullptr){
+                slow=slow->next;
+                fast=fast->next->next;
+                if(slow==fast){
+                    break;
+                }
+            }
+            if(slow!=fast){
+                return nullptr;
+            }
+            slow=head;
+            int count=0;
+            ListNode *node=fast;
+            while(slow!=fast){
+                if(fast==node){
+                    count++; // m
+                }
+                slow=slow->next;
+                fast=fast->next;
+            }
+            return slow=fast;
+            
+            
+        }
+    };
+    ///count==m count which is m
